239a35ff32333cd1b73c3cc7e972bd15f8f53794
andrea costa <g.costa14@campus.uniurb.it>
2022-09-15 18:36:49 +0200

    trasformare csv in json

--- Monumenti_culturali_Bologna/server.js
+++ Monumenti_culturali_Bologna/server.js
@@ -1,24 +1,36 @@
-// chiamo il modulo express
-const express = require('express')
-// creo server
-const app = express()
-// 
+/******************************* richiamo tutti i moduli *******************************/
+const express = require('express')                              // chiamo il modulo express
+const app = express()                                           // creo server
+const res = require('ejs')                                      // chiamo il modulo ejs
+const fs = require('fs');                                       // chiamo il modulo fs
+const CSVtoJSON = require('csvtojson')
 
-// chiamo il modulo ejs
-const res = require('ejs')
-// chiamo il modulo fs
-const fs = require('fs');
+/***************************** conversione del file da csv a json *****************************/
+const csvfilepath = "views/DataStrutture.csv"
 
-// settaggi principali
+CSVtoJSON()
+    .fromFile(csvfilepath).then((json) => {
+
+    // scrittura del file json
+    fs.writeFileSync("strutture.json", JSON.stringify(json), "utf-8",(err) => {
+        if(err){
+            console.log(err)
+        }
+    });
+});
+
+// lettura del file Json
+let strutture = JSON.parse(fs.readFileSync("strutture.json"));
+
+/******************************* SETTAGGI PRINCIPALI *******************************/
 // Inizializzazione tramite express.
 app.use(express.static('public'));
 app.use(express.json());
 
-// Per le richieste json.
+// utilizzo dei pacchetti e delle richieste json
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));  // per fare leggere i dati dal form
 
-///?????????????????????????
 // definizione del motore di ricerca html
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -31,20 +43,17 @@ var server = app.listen(app.get('port'), function() {
   console.log('The server listening on port ' + server.address().port);
 });
 
-/* ----------------------- END POINT GET --------------------- */ 
-// 1° ENDPOINT GET. Necessario per entrare nella pagina principale (index)
-app.get('/', (req, res) => {
-    res.render('./views/index.html');
+/******************************* END POINT GET *******************************/ 
+// 1° ENDPOINT GET. Necessario per viasualizzare tutte le strutture
+app.get('/strutture', (req, res) => {
+    if(strutture.length == 0){
+        res.sendStatus(404);
+    } else {
+        res.send(strutture)
+    }
 })
 
-// 2° ENDPOINT GET. Necessario per caricare i dati dal file csv
-app.get('/datacsv', (req, res) => {
-    console.log("Carico dati")
-    res.type('text/csv').sendFile(__dirname + '/views/DataStrutture.csv');
-})
-
-
-// 3° ENDPOINT GET. Necessario per la visualizzazione delle varie pagine
+// 2° ENDPOINT GET. Necessario per la visualizzazione delle varie pagine
 app.get('/', (req, res) => {
     res.render('./views/index.html');
 })
@@ -71,61 +80,83 @@ app.get('*', (req, res) => {
 
 
 // 4° ENDPOINT GET. Necessario per ricerca e acquisizione dati
-app.get("/DataStrutture/:param", (req, res) => {
+/*app.get("/ricerca", (req, res) => {
   let strutture = CSVToArray(fileSystem.readFileSync("./views/DataStrutture.csv"));
-  console.log("OK" + dataArray);
+  console.log("OK" + strutture);
   if(strutture.includes(param)){
-    console.log(dataArray);
     let dataArray = [];
+    console.log("OK" + dataArray);
     strutture.forEach(elemento => {
         dataArray.push(elemento)
     });
+    // richiesta andata a buon fine
     res.status(200).send(JSON.stringify(dataArray));
+    res.render('./views/trovato.html');
   }
-});
+    
+});*/
 
 /* --------------- ENDPOINT: POST -------------- */
 // devo inserire una struttura e definisco il tipo di azione
 app.post("/inserimento", (req, res) => {
      
      // salvo i dati del csv in strutture
-     let strutture = CSVToArray(fs.readFileSync("./views/DataStrutture.csv"));
-     console.log(strutture);
-          
-     // acquisisco i dati
-     let denominazione = req.body.denominazione;
-     let quartiere     = req.body.quartiere;
-     let indirizzo     = req.body.indirizzo;
-     let categoria     = req.body.categoria;
-     let descrizione   = req.body.descrizione;
-     let latitudine    = req.body.latitudine;
-     let longitudine   = req.body.longitudine;
+    /* let strutture = CSVToArray(fs.readFileSync("./views/DataStrutture.csv"));*/
+    
+    const ws = fs.createWriteStream("views/DataStrutture.csv")
+
+    /*let csvWrite = wtiteCsv({
+        path:'views/DataStrutture.csv',
+        header: [
+           { id: "denominazione", title: "Denominazione" },
+           { id: "quartiere", title: "Quartiere" },
+           { id: "indirizzo", title: "Indirizzo" },
+           { id: "categoria", title: "Categoria" },
+           { id: "descrizione", title: "Descrizione" },
+           { id: "latitudine", title: "Latitudine" }, 
+           { id: "longitudine", title: "Longitudine" },          
+        ]
+    }, { append: true });*/
+
+    // acquisisco i dati
+    let denominazione = req.body.denominazione;
+    let quartiere     = req.body.quartiere;
+    let indirizzo     = req.body.indirizzo;
+    let categoria     = req.body.categoria;
+    let descrizione   = req.body.descrizione;
+    let latitudine    = req.body.latitudine;
+    let longitudine   = req.body.longitudine;
 
      // creo un array contenente le info relative ad una riga
     // che dovrà essere inserita nell'array appena creato
-    let nuovaStruttura = '/n' +
+    let nuovaStruttura = [
         denominazione + ";" + 
         quartiere     + ";" +
         indirizzo     + ";" +
         categoria     + ";" +
         descrizione   + ";" +
         latitudine    + ";" +
-        longitudine;
-  
-     console.log(nuovaStruttura);
+        longitudine];
 
      // inserisco la nuova struttura nel file csv
-     strutture.push(nuovaStruttura);
+     //strutture.push(nuovaStruttura);
 
      // appendo il file.
-     fs.writeFileSync("./views/DataStrutture.csv", nuovaStruttura, {flag:'a'});
-     console.log(strutture);
-     res.status(200).send(nuovaStruttura);
+     //fs.writeFileSync("./views/DataStrutture.csv", nuovaStruttura, {flag:'a'});
+     /*csvWrite
+        .writeRecords(nuovaStruttura)
+        .then(() =>  console.log('The CSV file was written successfully'));*/
+    ws.append(nuovaStruttura);
+    fastcsv.write(ws, {headers:true}).pipe(ws);
+
+    console.log(nuovaStruttura);
+    console.log(ws);
+    res.status(200).send(nuovaStruttura);
    
 });
 
 /* --------------- ENDPOINT: DELETE -------------- */
-app.delete("/rimuovi/:nome", (req, res) => {
+/*app.delete("/rimuovi/:nome", (req, res) => {
     // definizione del nuovo array nella quale inserire i dati presi dal csv
     let strutture = CSVToArray(fileSystem.readFileSync("./views/DataStrutture.csv"));
     let nome = req.params.nome;
@@ -143,7 +174,7 @@ app.delete("/rimuovi/:nome", (req, res) => {
 /* Preso da https://gist.github.com/luishdez/644215 */
     // This will parse a delimited string into an array of arrays. 
     // The default delimiter is the comma, but this can be overriden in the second argument.
-    function CSVToArray( strData, strDelimiter ){
+    /*function CSVToArray( strData, strDelimiter ){
         // Check to see if the delimiter is defined. If not, then default to comma.
         strDelimiter = (strDelimiter || ",");
 
@@ -197,5 +228,4 @@ app.delete("/rimuovi/:nome", (req, res) => {
         }
         // Return the parsed data.
         return( arrData );
-    }
-
+    }*/
